8355df19e8f4c12563240f0ed35487b57cebc054
Rizkychi <rizkynhae@gmail.com>
2019-11-28 16:30:17 +0700

    starting template mdbootstrap

---
+++ config/conf.php
@@ -0,0 +1,4 @@
+<?php
+    $host = $_SERVER['HTTP_HOST'];
+    $uri  = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+?>
---
+++ page/register.php
@@ -0,0 +1,171 @@
+<?php
+    if (isset($_GET['u'])) {
+        if ($_GET['u'] == 'lecturer') {
+            $user = 'dosen';
+        } else {
+            $user = 'mahasiswa';
+        }
+    } else {
+        header("Location: http://$host$uri/index.php");
+    }
+?>
+<body class="coworking-page">
+<!-- Main navigation -->
+<header>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg scrolling-navbar navbar-light z-depth-0 white ml-md-4 mr-md-3 smooth-scroll">
+    <a class="navbar-brand purple-pastel" href="#">
+        <strong><span class="font-weight-bold">A L I C E</span><span
+            class="font-weight-bold pink-pastel">.</span></strong>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-4"
+        aria-controls="navbarSupportedContent-4" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent-4">
+        <ul class="navbar-nav ml-auto text-uppercase smooth-scroll">
+        <li class="nav-item">
+            <a class="nav-link heather-color" href="#about" data-offset="100">
+            <strong>About us</strong>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link heather-color" href="#articles" data-offset="100">
+            <strong>Articles</strong>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link heather-color" href="#offer" data-offset="100">
+            <strong>Memberships</strong>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link heather-color" href="#contact" data-offset="100">
+            <strong>Contact us</strong>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link pt-0-1" href="#contact" data-offset="100">
+            <button type="button" class="btn btn-outline-purple-pastel btn-rounded btn-md z-depth-0 m-0 pt-2">Masuk <i class="fas fa-angle-double-right"></i></button>
+            </a>
+        </li>
+        </ul>
+    </div>
+    </nav>
+    <!-- Navbar -->
+
+
+    <div class="container-fluid">
+
+        <div class="row d-flex justify-content-center h-100 mx-md-5">
+
+        <div class="col-lg-4 col-xl-5 col-flex mt-lg-0 pt-3">
+
+            <!-- Material form register -->
+            <div class="card">
+
+            <h5 class="card-header purple-gradient white-text text-center py-4 font-weight-bold">
+                <strong>Daftar Akun <?php echo ucwords($user); ?></strong>
+            </h5>
+
+            <!--Card content-->
+            <div class="card-body px-lg-5 pt-0">
+
+                <!-- Form -->
+                <form id="regForm-alice" class="text-center" style="color: #757575;" action="#!">
+                   
+                    <div class="row">
+                        <div class="col">
+                            <!-- NIM -->
+                            <div class="md-form">
+                                <input type="text" id="nim" class="form-control">
+                                <label for="nim">NIM</label>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <!-- Name -->
+                            <div class="md-form">
+                                <input type="text" id="fname" class="form-control">
+                                <label for="fname">Nama Lengkap</label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+                            <!-- Email -->
+                            <div class="md-form mt-2">
+                                <input type="email" id="email" class="form-control">
+                                <label for="email">E-mail</label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col">
+                            <!-- Password -->
+                            <div class="md-form mt-2">
+                                <input type="password" id="password" class="form-control">
+                                <label for="password">Kata Sandi</label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-8 mt-3">
+                            <!-- Default inline 1-->
+                            <div class="custom-control custom-radio custom-control-inline">
+                                <input type="radio" class="custom-control-input" id="rdbGender1" name="gender">
+                                <label class="custom-control-label" for="rdbGender1">Laki-laki</label>
+                            </div>
+
+                            <!-- Default inline 2-->
+                            <div class="custom-control custom-radio custom-control-inline">
+                                <input type="radio" class="custom-control-input" id="rdbGender2" name="gender">
+                                <label class="custom-control-label" for="rdbGender2">Perempuan</label>
+                            </div>
+                        </div>
+
+                        <div class="col-4">
+                            <!-- Name -->
+                            <div class="md-form mt-2">
+                                <input type="text" id="date-picker" class="form-control datepicker">
+                                <label for="date-picker">Tanggal Lahir</label>
+                            </div>
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn purple-gradient font-weight-bold">Daftar</button>
+
+
+                    <hr>
+
+                    <!-- Terms of service -->
+                    <p>Sudah punya akun? Masuk 
+                        <a href="../index.php">disini.</a>
+
+                </form>
+                <!-- Form -->
+
+            </div>
+
+            </div>
+            <!-- Material form register -->
+
+        </div>
+
+        <div class="col-lg-8 col-xl-7 pt-lg-4">
+
+            <div class="view">
+            <img src="https://mdbootstrap.com/img/illustrations/graphics(2).png" class="img-fluid" alt="smaple image">
+            </div>
+
+        </div>
+
+        </div>
+
+    </div>
+    <!-- Intro -->
+
+</header>
