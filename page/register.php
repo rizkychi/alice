ec1aeec18324a8ad726d3aa370181e0d8f888e5c
Rizkychi <rizkynhae@gmail.com>
2019-12-08 20:24:31 +0700

    perbaikan link

--- page/register.php
+++ page/register.php
@@ -149,7 +149,7 @@
 
                     <!-- Terms of service -->
                     <p>Sudah punya akun? Masuk 
-                        <a href="../index.php">disini.</a>
+                        <a href="?p=landing#login">disini.</a>
 
                 </form>
                 <!-- Form -->
