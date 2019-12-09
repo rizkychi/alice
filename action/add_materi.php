8a0d8f7dd6fe87e1c97db91c638d36812ce310ea
awowawa <imadlz15@gmail.com>
2019-12-09 13:41:40 +0700

    materi

--- action/add_materi.php
+++ action/add_materi.php
@@ -1,19 +1,45 @@
 <?php
     include '../config/conf.php';
-            $id     = $_POST['materiID'];
-            $user   = $_POST['materiUser'];
-            $subject= $_POST['materiName'];
-            $course = $_POST['course'];
-            $content= $_POST['materiContent'];
-            $date   = $_POST['materiDate'];
-            $button = $_POST['button'];
 
-            // $nama_file = $_FILES["materiFile"]["name"];
-            // move_uploaded_file($_FILES["materiFile"]["tmp_name"],"../filemateri".$nama_file);
+    // $act = $_GET['act'];
 
-            if($button = "TAMBAH") {
-                $query = mysqli_query($conn, "INSERT INTO tb_material (material_id, material_course, material_user, material_subject, material_content, material_date) VALUES ('$id','$course', '$user','$subject','$content', '$date')");
-            }
+    // if ($act == 'add') {
+    //     if ($_POST) {
+    //         $user   = $_POST['materiUser'];
+    //         $subject= $_POST['materiName'];
+    //         $course = $_POST['course'];
+    //         $content= $_POST['materiContent'];
 
-                header('Location: ../?p=materi');
+    //         $nama_file = $_FILES["materiFile"]["name"];
+    //         move_uploaded_file($_FILES["materiFile"]["tmp_name"],"../filemateri/".$nama_file);
+
+    //         $query = mysqli_query($conn, "INSERT INTO tb_material (material_course, material_user, material_subject, material_content, material_attachment) VALUES ('$course', '$user','$subject','$content', '$nama_file')");
+
+    //         if (!$query) {
+    //             echo "<script>alert('gagal')</script>";
+    //         } else 
+    //         echo "<script>alert('sukses')</script>";{
+    //             // header('Location: ../?p=admin&v=course');
+    //         }
+    //     }
+    // }
+    
+    $user   = $_POST['materiUser'];
+    $subject= $_POST['materiName'];
+    $course = $_POST['course'];
+    $content= $_POST['materiContent'];
+
+    $nama_file = $_FILES["materiFile"]["name"];
+    move_uploaded_file($_FILES["materiFile"]["tmp_name"],"../filemateri/".$nama_file);
+
+    if($button = "TAMBAH") {
+        $query = mysqli_query($conn, "INSERT INTO tb_material (material_course, material_user, material_subject, material_content, material_attachment) VALUES ('$course', '$user','$subject','$content', '$nama_file')");
+    }
+
+    if (!$query) {
+        echo "<script>alert('gagal menambahkan')</script>";
+    } else {
+        echo "<script>alert('data ditambahkan')</script>";
+        // header('Location: ../page/forum.php');
+    }
 ?>
