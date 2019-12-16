9e7af5531f5101b08d14295ccf7d7574a3255bd2
Rizkychi <rizkynhae@gmail.com>
2019-12-16 10:24:16 +0700

    benerin email verifikasi

--- action/do_register.php
+++ action/do_register.php
@@ -29,18 +29,23 @@ if ($_POST){
         $sql = "INSERT INTO tb_user (user_id, user_email, user_password, user_name, user_dob , user_gender,user_role) 
         VALUES ('$id','$email',md5('$pass'),'$fname','$date','$gender','$role')";
         
-        $simpan = mysqli_query($conn,$sql); } 
+        $simpan = mysqli_query($conn,$sql); 
 
-    if ($simpan) {
-        require_once 'sendmail.php';
-        $_SESSION['register_success'] = true;
-        header('Location: ../?p=register&u='.$_POST['userRole']);
-        //echo "<script>location.replace('../?p=register&u=".$_POST['userRole']."')</script>";
-    } 
-    else 
-    { echo "GUAGAL COY"; 
+        if ($simpan) {
+            $_SESSION['user'] = $id;
+            $_SESSION['fname'] = $fname;
+            $_SESSION['email'] = $email; 
+            $_SESSION['activate'] = base64_encode(md5($pass)); //email verification
+            require_once 'sendmail.php';
     
-    } 
-    
+            $_SESSION['register_success'] = true;
+            header('Location: ../?p=register&u='.$_POST['userRole']);
+            //echo "<script>location.replace('../?p=register&u=".$_POST['userRole']."')</script>";
+        } 
+        else 
+        { echo "GUAGAL COY"; 
+        
+        } 
+    }     
 }
 ?>
