a6568be98c0889543f09cec8b7cf03c94b0c2255
Rizkychi <rizkynhae@gmail.com>
2019-12-17 06:36:06 +0700

    update img profile

--- action/delete_picture.php
+++ action/delete_picture.php
@@ -1,13 +1,23 @@
 <?php
     session_start();
-    require '../config/conf.php';
+    require_once '../config/conf.php';
     
     if (isset($_SESSION['user'])) {
         $uid = $_SESSION['user'];
     }
 
-    //query
-    $query = mysqli_query($conn, "UPDATE tb_user SET user_photo = 'avatar.png' WHERE user_id = '$uid'");
+    //query and delete
+    $path    = '../img/alice-img/';
+    $query   = mysqli_query($conn, "SELECT user_photo FROM tb_user WHERE user_id = '$uid'");
+    $result  = mysqli_fetch_row($query);
+    $imgName = $result[0];
+
+    //query default
+    $query   = mysqli_query($conn, "UPDATE tb_user SET user_photo = 'avatar.png' WHERE user_id = '$uid'");
+    
+    if ($imgName != 'avatar.png') {
+        unlink($path.$imgName);
+    }
 
     if (!$query) {
         echo "<script>alert('gagal')</script>";
--- action/upload_picture.php
+++ action/upload_picture.php
@@ -1,12 +1,13 @@
 <?php
     session_start();
-    require '../config/conf.php';
+    require_once '../config/conf.php';
 
     if (isset($_SESSION['user'])) {
         $uid = $_SESSION['user'];
     }
 
     //upload.php
+    $path = '../img/alice-img/';
 
     if(isset($_POST["image"]))
     {
@@ -18,9 +19,18 @@
 
         $data = base64_decode($image_array_2[1]);
 
-        $imageName = $uid . '.png';
+        $imageName = $uid . '_' . time() . '.png';
 
-        file_put_contents('../img/alice-img/'.$imageName, $data);
+        //query and delete 
+        $query   = mysqli_query($conn, "SELECT user_photo FROM tb_user WHERE user_id = '$uid'");
+        $result  = mysqli_fetch_row($query);
+        $imgName = $result[0];
+        
+        if ($imgName != 'avatar.png') {
+            unlink($path.$imgName);
+        }
+
+        file_put_contents($path.$imageName, $data);
 
         //query
         $query = mysqli_query($conn, "UPDATE tb_user SET user_photo = '$imageName' WHERE user_id = '$uid'");
