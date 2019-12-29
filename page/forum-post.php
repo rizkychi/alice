fbca287502eb3822fa0cc9a1706b5c787b17e626
rizkikhns <rizkikhns@gmail.com>
2019-12-29 11:08:40 +0700

    thread update delete read comment list matkul sidebar

--- page/forum-post.php
+++ page/forum-post.php
@@ -1,41 +1,14 @@
 <?php
-    if (isset($_GET['id'])) {
+        require_once 'config/conf.php';
         $post_id = $_GET['id'];
-       // $query  = mysqli_query($conn, "SELECT * FROM tb_forum_post WHERE postID = $id");
-       $query  = mysqli_query($conn, "SELECT post_course, post_user, post_subject, post_content, post_date FROM tb_forum_post JOIN tb_course ON tb_forum_post.post_course = tb_course.course_id WHERE post_id = '$post_id'");
-            $result = mysqli_fetch_array($query);
-            $user_id = $result['post_user'];
-            $subject= $result['post_subject'];
-            $content= $result['post_content'];
-            $course = $result['post_course'];
-            $post_date = $result['post_date'];
-    }    
-?>
-
-<!-- comment -->
-<?php
-    // if (isset($_GET['act'])){
-    //     $act = $_GET['act'];
-    // } else {
-    //     //die();
-    // }
-
-    // if ($act == 'add') {
-    //     $id     = '';
-    //     $user   = $_SESSION['user'];
-    //     $comments = '';
-    //     $button = 'Post';
-    // } else if ($act == 'update') {
-    //     if (isset($_GET['comment_id'])) {
-    //         $id     = $_GET['comment_id'];
-    //         $query  = mysqli_query($conn, "SELECT * FROM tb_forum_comment WHERE comment_id = $id");
-    //         $result = mysqli_fetch_array($query);
-    //         $user   = $result['comment_user'];
-    //         $button = 'Post';
-    //     }
-    // }
-
-
+        $query  = mysqli_query($conn, "SELECT tb_forum_post.post_course, tb_forum_post.post_user, tb_forum_post.post_subject, tb_forum_post.post_content, tb_forum_post.post_date, tb_course.course_name FROM tb_forum_post JOIN tb_course ON tb_forum_post.post_course = tb_course.course_id WHERE post_id = '$post_id'");
+        $result = mysqli_fetch_array($query);
+        $user_id = $result['post_user'];
+        $subject= $result['post_subject'];
+        $content= $result['post_content'];
+        $course = $result['course_name'];
+		$post_date = $result['post_date'];  
+        $row=mysqli_fetch_assoc($query);  
 ?>
 
 
@@ -49,9 +22,9 @@
                 <div class="row justify-content-between">
                     <div class="col-md-4 mb-3">
                         <p class="font-small dark-grey-text mb-1">
-                            <strong>Penulis:</strong><?php echo $user_id; ?></p>
+                            <strong>Penulis: </strong><?php echo $user_id; ?></p>
                         <p class="font-small grey-text">
-                        <?php echo $post_date; ?></p>
+                            <strong>Pada tanggal </strong><?php echo $post_date; ?></p>
                         <a>
                             <span class="badge badge-danger"><?php echo $course; ?></span>
                         </a>
@@ -62,10 +35,9 @@
                         aria-haspopup="true" aria-expanded="false"><i class="fas fa-ellipsis-h" style="font-size:20px;"></i></a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="alice-dropdown">
-                            <a class="dropdown-item" href="#">Ubah</a>
-                            <a class="dropdown-item" href='action/delete_post.php?post_id=$row[post_id]'>Hapus</a>
-                            
-                            
+                            <a class="dropdown-item" href="?p=forum-form&id=<?php echo $post_id; ?>&act=update">Edit</a>
+                            <a class="dropdown-item" href="action/delete_thread.php?id=<?php echo $post_id; ?>" onclick="javascript: return confirm('Yakin Hapus?')" >Hapus</a>
+
                         </div>
                         <!-- Basic dropdown -->
                     </div>
@@ -119,17 +91,23 @@
 
             <div class="row px-4">
                 <h4>Komentar</h4>
-                <form action="action/_forumcomment.php?act=<?php echo $act; ?>" method="post" class="col-md-12">
+                <form action="" method="post" class="col-md-12">
                     <div class="form-group purple-border mt-2 mb-1">
                         <textarea class="form-control" name="comments" rows="3" placeholder="Tulis komentar..." required></textarea>
                     </div>
-                    <input class="btn btn-secondary btn-sm mx-0 my-1" type="submit" value="Kirim">
-                </form>
+                <!--  <button type="submit" name = "button" class="btn btn-secondary btn-sm mx-0 my-1" value="<?php echo $button; ?>"</button> -->
+                <input type="submit" name= "button" class="btn btn-secondary btn-sm mx-0 my-1" value="KIRIM"/>
+             </form>
             </div>
             <hr>
 
-            <?php
-                for ($i=0; $i < 2; $i++) { 
+            <?php 
+                    $sql =  "SELECT tb_forum_post.post_id, tb_forum_comment.comment_id, tb_forum_comment.comment_post, tb_forum_comment.comment_user, tb_forum_comment.comment_content, tb_forum_comment.comment_date from tb_forum_post JOIN tb_forum_comment on tb_forum_post.post_id = tb_forum_comment.comment_post where post_id = $post_id";
+                    $result = mysqli_query($conn, $sql);
+                    if (mysqli_num_rows($result) > 0) {
+                                        // output data of each row
+                                        while($row = mysqli_fetch_array($result)) {
+                                            //var_dump($row);
                     ?>
                         <div class="row px-4">
                             <!-- Comments -->
@@ -144,12 +122,10 @@
                                     </div>
                                 </div>
                                 <div class="w-100">
-                                    <h6 class="h6 mb-0">Nama orang
-                                        <span class="text-black-50 ml-2 font-small">04/04/2017</span>
+                                    <h6 class="h6 mb-0"><?php $row[3]; ?>
+                                        <span class="text-black-50 ml-2 font-small"><?php echo $row[5]; ?></span>
                                     </h6>
-                                    <p class="dark-grey-text article">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                        eiusmod tempor incididunt
-                                        ut labore et dolore magna aliqua. </p>
+                                    <p class="dark-grey-text article"><?php echo $row[4]; ?></p>
                                 </div>
                             </div>
                             <!-- Comments -->
@@ -157,6 +133,7 @@
 
                         <hr>
                     <?php
+                }
                 }    
             ?>
         </div>
